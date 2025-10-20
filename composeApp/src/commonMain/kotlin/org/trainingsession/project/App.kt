58a9e2a594cc8c130dccc9b355c1edf1052b9108
palamarchukhome <palamarchuk.office@gmail.com>
2025-10-20 23:19:36 +0300

    Use new screen within common module for each platform.

--- composeApp/src/commonMain/kotlin/org/trainingsession/project/App.kt
+++ composeApp/src/commonMain/kotlin/org/trainingsession/project/App.kt
@@ -1,29 +1,29 @@
 package org.trainingsession.project
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
-
-import trainingsession.composeapp.generated.resources.Res
-import trainingsession.composeapp.generated.resources.compose_multiplatform
+import org.trainingsession.project.mainScreen.ProgramSelectionScreen
+import org.trainingsession.project.mainScreen.WorkoutPlayerScreen
+import org.trainingsession.project.mainScreen.WorkoutProgramPresentation
+import org.trainingsession.project.mainScreen.theme.AppTheme
 
 @Composable
 @Preview
 fun App() {
-    MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
+    AppTheme {
+        var selectedProgram by remember { mutableStateOf<WorkoutProgramPresentation?>(null) }
+
         Column(
             modifier = Modifier
                 .background(MaterialTheme.colorScheme.primaryContainer)
@@ -31,18 +31,15 @@ fun App() {
                 .fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
+            if (selectedProgram == null) {
+                ProgramSelectionScreen(
+                    onProgramSelected = { selectedProgram = it }
+                )
+            } else {
+                WorkoutPlayerScreen(
+                    program = selectedProgram!!,
+                    onBackClick = { selectedProgram = null }
+                )
             }
         }
     }
