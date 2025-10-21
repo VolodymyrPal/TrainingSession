9c1b360121a9093f9d55be2787deda66f44497e3
palamarchukhome <palamarchuk.office@gmail.com>
2025-10-21 18:09:40 +0300

    Wrap app with navigation library.

--- composeApp/src/commonMain/kotlin/org/trainingsession/project/App.kt
+++ composeApp/src/commonMain/kotlin/org/trainingsession/project/App.kt
@@ -1,18 +1,24 @@
 package org.trainingsession.project
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import kotlinx.coroutines.delay
 import org.jetbrains.compose.ui.tooling.preview.Preview
+import org.trainingsession.project.mainScreen.AppRoutes
+import org.trainingsession.project.mainScreen.ExercisePresentation
 import org.trainingsession.project.mainScreen.ProgramSelectionScreen
 import org.trainingsession.project.mainScreen.WorkoutPlayerScreen
 import org.trainingsession.project.mainScreen.WorkoutProgramPresentation
@@ -21,25 +27,72 @@ import org.trainingsession.project.mainScreen.theme.AppTheme
 @Composable
 @Preview
 fun App() {
-    AppTheme {
-        var selectedProgram by remember { mutableStateOf<WorkoutProgramPresentation?>(null) }
 
-        Column(
-            modifier = Modifier
-                .background(MaterialTheme.colorScheme.primaryContainer)
-                .safeContentPadding()
-                .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
+    val theme = remember { mutableStateOf(false) }
+    val navController = rememberNavController()
+
+    AppTheme(
+        darkTheme = theme.value
+    ) {
+        Surface(
+            modifier = Modifier,
+            color = MaterialTheme.colorScheme.surfaceContainerHigh,
+            contentColor = Color.Transparent
         ) {
-            if (selectedProgram == null) {
-                ProgramSelectionScreen(
-                    onProgramSelected = { selectedProgram = it }
+            var selectedProgram by remember {
+                mutableStateOf<WorkoutProgramPresentation?>(
+                    WorkoutProgramPresentation(
+                        3,
+                        "Дыхательные практики",
+                        "Техники для снятия стресса и концентрации",
+                        listOf(
+                            ExercisePresentation(
+                                "Диафрагмальное дыхание",
+                                60,
+                                "Глубокий вдох животом"
+                            ),
+                            ExercisePresentation(
+                                "Квадратное дыхание",
+                                90,
+                                "Вдох-задержка-выдох-задержка"
+                            ),
+                            ExercisePresentation(
+                                "Расслабляющее дыхание",
+                                120,
+                                "Медленный выдох через рот"
+                            )
+                        )
+                    )
                 )
-            } else {
-                WorkoutPlayerScreen(
-                    program = selectedProgram!!,
-                    onBackClick = { selectedProgram = null }
-                )
+            }
+
+            LaunchedEffect(Unit) {
+                while (true) {
+                    delay(5000)
+                    theme.value = !theme.value
+                }
+            }
+
+            NavHost(
+                modifier = Modifier.safeContentPadding().fillMaxSize(),
+                navController = navController,
+                startDestination = AppRoutes.ProgramListScreen
+            ) {
+
+                composable<AppRoutes.ProgramListScreen> {
+                    ProgramSelectionScreen(
+                        onProgramSelected = { navController.navigate(AppRoutes.ChosenProgramScreen) }
+                    )
+                }
+
+                composable<AppRoutes.ChosenProgramScreen> {
+                    WorkoutPlayerScreen(
+                        program = selectedProgram!!,
+                        onBackClick = {
+                            navController.navigate(AppRoutes.ProgramListScreen)
+                        }
+                    )
+                }
             }
         }
     }
