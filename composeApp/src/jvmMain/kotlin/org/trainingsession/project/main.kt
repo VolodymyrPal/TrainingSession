1e6d49877fd17e5211a51546987fea58a478874b
palamarchukhome <palamarchuk.office@gmail.com>
2025-10-22 21:07:07 +0300

    Add koin initialization for PC.

--- composeApp/src/jvmMain/kotlin/org/trainingsession/project/main.kt
+++ composeApp/src/jvmMain/kotlin/org/trainingsession/project/main.kt
@@ -2,8 +2,10 @@ package org.trainingsession.project
 
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import org.koin.core.context.startKoin
 
 fun main() = application {
+    startKoin { modules(appModule) }
     Window(
         onCloseRequest = ::exitApplication,
         title = "TrainingSession",
