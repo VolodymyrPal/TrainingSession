87fe3b1d4c2197d57637b56f8139da89d4db5b72
palamarchukhome <palamarchuk.office@gmail.com>
2025-10-22 21:06:18 +0300

    Add koin library for dependency injection.

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -45,6 +45,11 @@ kotlin {
             implementation(libs.androidx.lifecycle.runtimeCompose)
             implementation("org.jetbrains.androidx.navigation:navigation-compose:2.9.1")
             implementation(libs.kotlinx.serialization.json)
+            implementation("io.insert-koin:koin-compose:4.1.1")
+            implementation("io.insert-koin:koin-compose-viewmodel:4.1.1")
+            implementation("io.insert-koin:koin-compose-viewmodel-navigation:4.1.1")
+
+            implementation("io.insert-koin:koin-core:4.1.1") //!!!
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
