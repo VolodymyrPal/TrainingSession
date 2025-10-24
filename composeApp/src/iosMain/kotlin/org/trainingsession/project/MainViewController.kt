d5a8840b779d850c073e083c61a4824093028da8
palamarchukhome <palamarchuk.office@gmail.com>
2025-10-24 23:41:17 +0300

    Move di to separate holder.

--- composeApp/src/iosMain/kotlin/org/trainingsession/project/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/org/trainingsession/project/MainViewController.kt
@@ -1,6 +1,7 @@
 package org.trainingsession.project
 
 import androidx.compose.ui.window.ComposeUIViewController
+import org.trainingsession.project.di.initKoin
 
 fun MainViewController() = ComposeUIViewController(
     configure = {
