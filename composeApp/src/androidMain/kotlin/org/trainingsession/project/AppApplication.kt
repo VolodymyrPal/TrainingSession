484829fc294a0440b7a26ae36143a75248a534b3
palamarchukhome <palamarchuk.office@gmail.com>
2025-10-24 23:43:33 +0300

    Remove Greeting class, koin folder.

--- composeApp/src/androidMain/kotlin/org/trainingsession/project/AppApplication.kt
+++ composeApp/src/androidMain/kotlin/org/trainingsession/project/AppApplication.kt
@@ -1,6 +1,7 @@
 package org.trainingsession.project
 
 import android.app.Application
+import org.trainingsession.project.di.initKoin
 
 class AppApplication : Application() {
     override fun onCreate() {
