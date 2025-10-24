15f8aa5687ec9b298a44400e1a000336f133e8d1
palamarchukhome <palamarchuk.office@gmail.com>
2025-10-24 23:38:51 +0300

    Actual implementation for java platform (PC).

--- composeApp/src/jvmMain/kotlin/org/trainingsession/project/Platform.jvm.kt
+++ composeApp/src/jvmMain/kotlin/org/trainingsession/project/Platform.jvm.kt
@@ -1,7 +1,5 @@
 package org.trainingsession.project
 
-class JVMPlatform: Platform {
-    override val name: String = "Java ${System.getProperty("java.version")}"
+actual class Platform actual constructor() {
+    actual val name: String = "Java ${System.getProperty("java.version")}"
 }
-
-actual fun getPlatform(): Platform = JVMPlatform()
