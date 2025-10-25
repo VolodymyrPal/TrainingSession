6e1a87e20917ec7f04afd57bd08d3511ba0c2d4a
palamarchukhome <palamarchuk.office@gmail.com>
2025-10-25 23:02:51 +0300

    Pass realization of lambda from swift code directly within ios initialization.

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,7 +1,14 @@
 import SwiftUI
+import ComposeApp
 
 @main
 struct iOSApp: App {
+    init() {
+        Platform_iosKt.setNativeWorkLambda {
+            NativeActionBridge.doWork()
+        }
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
