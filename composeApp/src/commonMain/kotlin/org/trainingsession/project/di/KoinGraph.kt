3e0e6143f11a57bb6f24b4a98364ee00144b14a8
palamarchukhome <palamarchuk.office@gmail.com>
2025-10-26 23:49:23 +0200

    Better function to init koin.

--- composeApp/src/commonMain/kotlin/org/trainingsession/project/di/KoinGraph.kt
+++ composeApp/src/commonMain/kotlin/org/trainingsession/project/di/KoinGraph.kt
@@ -1,11 +1,22 @@
 package org.trainingsession.project.di
 
+import org.koin.core.annotation.Module
+import org.koin.core.annotation.Single
 import org.koin.core.context.startKoin
 import org.koin.core.module.dsl.singleOf
 import org.koin.core.module.dsl.viewModel
+import org.koin.dsl.KoinAppDeclaration
 import org.koin.dsl.module
+import org.koin.ksp.generated.module
 import org.trainingsession.project.Platform
-import org.trainingsession.project.mainScreen.viewModels.ProgramsScreenViewModel
+import org.trainingsession.project.presentation.viewModels.ProgramsScreenViewModel
+
+fun initKoin(config: KoinAppDeclaration? = null) {
+    startKoin {
+        config?.invoke(this)
+        modules(appModule, commonModule, CommonModule().module)
+    }
+}
 
 fun initKoin() = startKoin {
     modules(appModule, commonModule)
