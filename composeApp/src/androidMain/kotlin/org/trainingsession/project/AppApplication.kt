05d9d97ec8a142bffb785a9a1ffac36ca17293d8
palamarchukhome <palamarchuk.office@gmail.com>
2025-10-26 23:46:51 +0200

    Better koin initialization for android platform.

--- composeApp/src/androidMain/kotlin/org/trainingsession/project/AppApplication.kt
+++ composeApp/src/androidMain/kotlin/org/trainingsession/project/AppApplication.kt
@@ -5,13 +5,13 @@ import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 import org.trainingsession.project.di.appModule
 import org.trainingsession.project.di.commonModule
+import org.trainingsession.project.di.initKoin
 
 class AppApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-        startKoin {
+        initKoin {
             androidContext(this@AppApplication)
-            modules(appModule, commonModule)
         }
     }
 }
