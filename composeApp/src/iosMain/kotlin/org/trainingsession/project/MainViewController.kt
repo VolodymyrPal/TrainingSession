aac9d8a237e50d179d91a5f7ba36304e7ab81acb
palamarchukhome <palamarchuk.office@gmail.com>
2025-10-27 22:24:32 +0200

    Move koin injection within swift entry point.

--- composeApp/src/iosMain/kotlin/org/trainingsession/project/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/org/trainingsession/project/MainViewController.kt
@@ -1,14 +1,5 @@
 package org.trainingsession.project
 
 import androidx.compose.ui.window.ComposeUIViewController
-import org.koin.core.context.startKoin
-import org.trainingsession.project.di.appModule
-import org.trainingsession.project.di.commonModule
 
-fun MainViewController() = ComposeUIViewController(
-    configure = {
-        startKoin {
-            modules(appModule, commonModule)
-        }
-    }
-) { App() }
+fun MainViewController() = ComposeUIViewController { App() }
