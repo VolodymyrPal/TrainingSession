704732b7230c0ab42f906ab19fb49502b4bc8f90
palamarchukhome <palamarchuk.office@gmail.com>
2025-10-27 22:24:14 +0200

    Update koin graph, trying to figure out ksp generation for it.

--- composeApp/src/commonMain/kotlin/org/trainingsession/project/di/KoinGraph.kt
+++ composeApp/src/commonMain/kotlin/org/trainingsession/project/di/KoinGraph.kt
@@ -1,23 +1,26 @@
 package org.trainingsession.project.di
 
+import org.koin.core.annotation.KoinApplication
 import org.koin.core.annotation.Module
 import org.koin.core.annotation.Single
-import org.koin.core.context.startKoin
 import org.koin.core.module.dsl.singleOf
-import org.koin.core.module.dsl.viewModel
+import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.KoinAppDeclaration
 import org.koin.dsl.module
 import org.koin.ksp.generated.module
+import org.koin.ksp.generated.startKoin
 import org.trainingsession.project.Platform
 import org.trainingsession.project.presentation.viewModels.ProgramsScreenViewModel
 
 fun initKoin(config: KoinAppDeclaration? = null) {
-    startKoin {
+    MyApp.startKoin {
         config?.invoke(this)
-        modules(appModule, commonModule, CommonModule().module)
+        modules(commonModule, CommonModule().module)
     }
 }
 
+@KoinApplication(modules = [CommonModule::class])
+object MyApp
 
 @Module
 class CommonModule {
@@ -27,9 +30,21 @@ class CommonModule {
 }
 
 val commonModule = module {
+    viewModelOf(::ProgramsScreenViewModel)
     singleOf(::Platform)
 }
 
-val appModule = module {
-    viewModel { ProgramsScreenViewModel() }
+fun initKoinWithIos(config: KoinAppDeclaration? = null, iosDependencies: IOSDependencies) {
+    initKoin {
+        config?.invoke(this)
+        modules(
+            platformModule(iosDependencies)
+        )
+    }
+}
+
+interface IOSDependencies {}
+
+fun platformModule(deps: IOSDependencies) = module {
+    single<IOSDependencies> { deps }
 }
