4fd201a7f495224c0748db3788f1736fc95e5957
palamarchukhome <palamarchuk.office@gmail.com>
2025-10-27 22:26:59 +0200

    Update viewModel to disturb programs with koin annotation for code generation.

--- composeApp/src/commonMain/kotlin/org/trainingsession/project/presentation/viewModels/ProgramsScreenViewModel.kt
+++ composeApp/src/commonMain/kotlin/org/trainingsession/project/presentation/viewModels/ProgramsScreenViewModel.kt
@@ -1,6 +1,16 @@
 package org.trainingsession.project.presentation.viewModels
 
 import androidx.lifecycle.ViewModel
+import org.koin.android.annotation.KoinViewModel
+import org.trainingsession.project.domain.models.Program
+import org.trainingsession.project.domain.repository.ProgramRepository
 
-class ProgramsScreenViewModel : ViewModel() {
+@KoinViewModel
+class ProgramsScreenViewModel(
+    private val repository: ProgramRepository
+) : ViewModel() {
+
+    fun getPrograms(): List<Program> {
+        return repository.getPrograms()
+    }
 }
