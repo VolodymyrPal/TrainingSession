8325508649fccd19a2a1733500bbff94fdf7490e
palamarchukhome <palamarchuk.office@gmail.com>
2025-10-28 23:37:12 +0200

    Create platform instance every time while access.

--- composeApp/src/androidMain/kotlin/org/trainingsession/project/Platform.android.kt
+++ composeApp/src/androidMain/kotlin/org/trainingsession/project/Platform.android.kt
@@ -1,7 +1,9 @@
 package org.trainingsession.project
 
 import android.os.Build
+import org.koin.core.annotation.Factory
 
+@Factory
 actual class Platform actual constructor() {
     actual val name: String = "Android ${Build.VERSION.SDK_INT}"
 }
