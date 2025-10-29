7012760889bbe119866dbf259b70e02fb9ef8a3a
palamarchukhome <palamarchuk.office@gmail.com>
2025-10-29 18:48:29 +0200

    Provide ktor's Logger to log network calls.

--- composeApp/src/commonMain/kotlin/org/trainingsession/project/utils/AppLogger.kt
+++ composeApp/src/commonMain/kotlin/org/trainingsession/project/utils/AppLogger.kt
@@ -1,7 +1,17 @@
 package org.trainingsession.project.utils
 
+import io.ktor.client.plugins.logging.Logger
+
 expect object AppLogger {
     fun e(tag: String, message: String, throwable: Throwable?)
     fun d(tag: String, message: String)
     fun i(tag: String, message: String)
+}
+
+private const val KTOR_TAG = "KtorClient"
+
+object MyKtorLogger : Logger {
+    override fun log(message: String) {
+        AppLogger.d(KTOR_TAG, message)
+    }
 }
