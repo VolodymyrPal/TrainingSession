0826df34115883ed4b00d17c48da6f07b1073bfe
palamarchukhome <palamarchuk.office@gmail.com>
2025-10-29 18:45:17 +0200

    Update Koin module with including platform specific module + http client provider.

--- composeApp/src/commonMain/kotlin/org/trainingsession/project/di/KoinGraph.kt
+++ composeApp/src/commonMain/kotlin/org/trainingsession/project/di/KoinGraph.kt
@@ -1,12 +1,23 @@
 package org.trainingsession.project.di
 
+import io.ktor.client.HttpClient
+import io.ktor.client.plugins.DefaultRequest
+import io.ktor.client.plugins.cache.HttpCache
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
 import org.koin.core.annotation.ComponentScan
+import org.koin.core.annotation.Factory
 import org.koin.core.annotation.KoinApplication
 import org.koin.core.annotation.Module
 import org.koin.core.logger.Level
+import org.koin.core.scope.Scope
 import org.koin.dsl.KoinAppDeclaration
 import org.koin.dsl.module
 import org.koin.ksp.generated.startKoin
+import org.trainingsession.project.utils.MyKtorLogger
+
 
 fun initKoin(config: KoinAppDeclaration? = null) {
     MyApp.startKoin {
@@ -18,10 +29,30 @@ fun initKoin(config: KoinAppDeclaration? = null) {
 @KoinApplication(modules = [CommonModule::class])
 object MyApp
 
-@Module
+@Module(includes = [NativeModuleD::class])
 @ComponentScan("org.trainingsession.project")
 class CommonModule {
 
+    @Factory
+    fun provideHttpClient(): HttpClient = HttpClient() {
+        install(DefaultRequest) {
+
+        }
+        install(Logging) {
+            logger = MyKtorLogger
+        }
+        install(ContentNegotiation) {
+            json(
+                Json {
+                    ignoreUnknownKeys = true
+                    encodeDefaults = true
+                    prettyPrint = true
+                    isLenient = true  //TODO lenient for testing
+                }
+            )
+        }
+        install(HttpCache)
+    }
 
 }
 
