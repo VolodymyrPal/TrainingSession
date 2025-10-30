ed9de8cbd163a2fc095e05ac134a44870d8c82ec
palamarchukhome <palamarchuk.office@gmail.com>
2025-10-30 23:07:30 +0200

    Extend program screen View model.

--- composeApp/src/commonMain/kotlin/org/trainingsession/project/presentation/viewModels/ProgramsScreenViewModel.kt
+++ composeApp/src/commonMain/kotlin/org/trainingsession/project/presentation/viewModels/ProgramsScreenViewModel.kt
@@ -1,15 +1,16 @@
 package org.trainingsession.project.presentation.viewModels
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import io.ktor.client.HttpClient
-import io.ktor.client.request.get
-import io.ktor.client.statement.bodyAsText
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import org.koin.android.annotation.KoinViewModel
 import org.trainingsession.project.domain.models.Program
 import org.trainingsession.project.domain.repository.ProgramRepository
-import org.trainingsession.project.utils.AppLogger
+import org.trainingsession.project.presentation.models.WorkoutProgramPresentation
+import org.trainingsession.project.presentation.models.toPresentationList
 
 @KoinViewModel
 class ProgramsScreenViewModel(
@@ -17,10 +18,15 @@ class ProgramsScreenViewModel(
     private val httpClient: HttpClient
 ) : ViewModel() {
 
+    private val _state = MutableStateFlow(ProgramScreenState())
+    val state: StateFlow<ProgramScreenState> = _state.asStateFlow()
+
     init {
-        viewModelScope.launch {
-            val response = httpClient.get("https://ktor.io/docs/")
-            AppLogger.d("AppResponse: ", "Response: ${response.bodyAsText()}")
+        val programs = getPrograms().toPresentationList()
+        _state.update {
+            it.copy(
+                programs = programs
+            )
         }
     }
 
@@ -28,3 +34,9 @@ class ProgramsScreenViewModel(
         return repository.getPrograms()
     }
 }
+
+data class ProgramScreenState(
+    val programs: List<WorkoutProgramPresentation> = emptyList(),
+    val isLoading: Boolean = false,
+    val error: String? = null
+)
