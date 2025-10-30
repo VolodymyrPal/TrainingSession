6fc55c7b2812695fc5faa9c5ac3661320174f089
palamarchukhome <palamarchuk.office@gmail.com>
2025-10-30 23:05:28 +0200

    Real use for repository.

--- composeApp/src/commonMain/kotlin/org/trainingsession/project/data/repository/ProgramRepositoryImpl.kt
+++ composeApp/src/commonMain/kotlin/org/trainingsession/project/data/repository/ProgramRepositoryImpl.kt
@@ -1,20 +1,26 @@
 package org.trainingsession.project.data.repository
 
 import org.koin.core.annotation.Single
-import org.trainingsession.project.domain.models.Exercise
 import org.trainingsession.project.domain.models.Program
+import org.trainingsession.project.domain.models.ProgramExercise
+import org.trainingsession.project.domain.models.provideProgramExercise
+import org.trainingsession.project.domain.models.providePrograms
 import org.trainingsession.project.domain.repository.ProgramRepository
 
 @Single(binds = [ProgramRepository::class])
-class ProgramRepositoryImpl (
+class ProgramRepositoryImpl(
 
 ) : ProgramRepository {
     override fun getPrograms(): List<Program> {
-        return emptyList()
+        return providePrograms()
     }
 
-    override fun getExercises(program: Program): Exercise {
-        return Exercise("Test", 20, "Description")
+    override fun getProgram(id: Int): Program {
+        return providePrograms()[id.coerceIn(0, providePrograms().size)] //TODO
+    }
+
+    override fun getExercises(program: Program): ProgramExercise {
+        return provideProgramExercise()
     }
 
 }
