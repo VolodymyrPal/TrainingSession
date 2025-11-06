49369e619ab8b6fc093f26a729c14443b84f371e
palamarchukhome <palamarchuk.office@gmail.com>
2025-11-06 23:03:07 +0200

    For compatibility reason preview rollback to java 21.

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -15,7 +15,7 @@ plugins {
 kotlin {
     androidTarget {
         compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_23)
+            jvmTarget.set(JvmTarget.JVM_21)
         }
     }
 
@@ -124,8 +124,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
 }
 
