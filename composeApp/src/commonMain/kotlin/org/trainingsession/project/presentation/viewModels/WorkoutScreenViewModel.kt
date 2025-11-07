f772e2dc8370d7fc4579eb2b8a97af85f61a07de
palamarchukhome <palamarchuk.office@gmail.com>
2025-11-07 23:57:28 +0200

    Update screen state class, extend data class for stepper.

--- composeApp/src/commonMain/kotlin/org/trainingsession/project/presentation/viewModels/WorkoutScreenViewModel.kt
+++ composeApp/src/commonMain/kotlin/org/trainingsession/project/presentation/viewModels/WorkoutScreenViewModel.kt
@@ -1,11 +1,17 @@
 package org.trainingsession.project.presentation.viewModels
 
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableLongStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 import org.koin.android.annotation.KoinViewModel
 import org.koin.core.annotation.InjectedParam
@@ -14,8 +20,11 @@ import org.trainingsession.project.domain.repository.ProgramRepository
 import org.trainingsession.project.presentation.composables.SequentialProgressState
 import org.trainingsession.project.presentation.composables.Stepper
 import org.trainingsession.project.presentation.models.toPresentation
-import org.trainingsession.project.presentation.service.ProgressController
-import org.trainingsession.project.utils.AppLogger
+import kotlin.coroutines.cancellation.CancellationException
+import kotlin.time.Duration.Companion.milliseconds
+import kotlin.time.TimeMark
+import kotlin.time.TimeSource
+
 
 @KoinViewModel
 class WorkoutScreenViewModel(
@@ -90,8 +99,10 @@ class WorkoutScreenViewModel(
     }
 }
 
-open class AppStepper(
-    override val durationMS: Long = 10L
+data class AppStepper(
+    override val durationMS: Long = 10L,
+    override val elapsedTime: MutableState<Long> = mutableLongStateOf(0),
+    val progress: MutableState<Float> = mutableFloatStateOf(0f)
 ) : Stepper
 
 data class ExerciseScreenState(
@@ -102,5 +113,9 @@ data class ExerciseScreenState(
         name = "",
         programExercises = emptyList()
     ),
-    val progressState: SequentialProgressState<AppStepper> = SequentialProgressState(emptyList())
+    val progressState: SequentialProgressState<AppStepper> = SequentialProgressState(emptyList()),
+    val currentIndex: Int = 0,
+    val isPlaying: Boolean = false,
+    val isLoading: Boolean = true,
+    val stepperList: List<AppStepper> = emptyList()
 )
