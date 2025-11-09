b1474342cc0cf8a48faffb66ef132df173945058
palamarchukhome <palamarchuk.office@gmail.com>
2025-11-09 22:24:28 +0200

    Add class to display presentation layer exercise.

--- composeApp/src/commonMain/kotlin/org/trainingsession/project/presentation/models/ExercisePresentation.kt
+++ composeApp/src/commonMain/kotlin/org/trainingsession/project/presentation/models/ExercisePresentation.kt
@@ -1,7 +1,15 @@
 package org.trainingsession.project.presentation.models
 
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableLongStateOf
+
 data class ExercisePresentation(
     val name: String,
     val durationSeconds: Int,
-    val description: String
-)
+    val description: String,
+) : Stepper {
+    override val durationMS: Long = durationSeconds * 1000L
+    override val progress: MutableState<Float> = mutableFloatStateOf(0f)
+    override val elapsedTime: MutableState<Long> = mutableLongStateOf(0)
+}
