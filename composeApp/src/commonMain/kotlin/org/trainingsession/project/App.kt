eae9ed419504fcf5684828833ad1bcdffbabfd50
palamarchukhome <palamarchuk.office@gmail.com>
2025-11-10 22:26:27 +0200

    Pass view model within composable navigation block to keep screen separate from logic.

--- composeApp/src/commonMain/kotlin/org/trainingsession/project/App.kt
+++ composeApp/src/commonMain/kotlin/org/trainingsession/project/App.kt
@@ -5,22 +5,24 @@ import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.toRoute
-import kotlinx.coroutines.delay
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import org.koin.compose.koinInject
+import org.koin.compose.viewmodel.koinViewModel
+import org.koin.core.parameter.parametersOf
 import org.trainingsession.project.presentation.AppRoutes
 import org.trainingsession.project.presentation.screens.ProgramSelectionScreen
 import org.trainingsession.project.presentation.screens.WorkoutPlayerScreen
 import org.trainingsession.project.presentation.theme.AppTheme
+import org.trainingsession.project.presentation.viewModels.WorkoutScreenViewModel
 import org.trainingsession.project.utils.AppLogger
 
 @Composable
@@ -67,10 +69,17 @@ fun App() {
 
                 composable<AppRoutes.ChosenProgramScreen> { backstack ->
                     val route: AppRoutes.ChosenProgramScreen = backstack.toRoute()
+                    val viewModel: WorkoutScreenViewModel = koinViewModel(
+                        parameters = { parametersOf(1) }
+                    )
+                    val state = viewModel.state.collectAsStateWithLifecycle()
+
                     WorkoutPlayerScreen(
                         onBackClick = {
                             navController.navigate(AppRoutes.ProgramListScreen)
-                        }
+                        },
+                        screenState = state,
+                        event = viewModel::event
                     )
                 }
             }
