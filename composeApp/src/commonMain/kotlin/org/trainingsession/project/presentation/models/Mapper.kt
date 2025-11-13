347cd17c9f3c4bd584bf0ff41e8bd19d942e16e6
palamarchukhome <palamarchuk.office@gmail.com>
2025-11-13 22:42:54 +0200

    Map with new value class.

--- composeApp/src/commonMain/kotlin/org/trainingsession/project/presentation/models/Mapper.kt
+++ composeApp/src/commonMain/kotlin/org/trainingsession/project/presentation/models/Mapper.kt
@@ -1,15 +1,15 @@
 package org.trainingsession.project.presentation.models
 
 import org.trainingsession.project.domain.models.Program
-import org.trainingsession.project.domain.models.ProgramExercise
+import org.trainingsession.project.domain.models.Exercise
 
-fun ProgramExercise.toPresentation(): ExercisePresentation {
+fun Exercise.toPresentation(): ExercisePresentation {
     val durationInSeconds = this.duration.inWholeSeconds.toInt()
 
     return ExercisePresentation(
         name = this.name,
         durationSeconds = durationInSeconds,
-        description = this.description
+        description = this.description.value
     )
 }
 
@@ -18,7 +18,7 @@ fun Program.toPresentation(): WorkoutProgramPresentation {
         id = this.id,
         name = this.name,
         description = this.description,
-        exercisePresentations = this.programExercises.map { it.toPresentation() }
+        exercisePresentations = this.exercises.map { it.toPresentation() }
     )
 }
 
