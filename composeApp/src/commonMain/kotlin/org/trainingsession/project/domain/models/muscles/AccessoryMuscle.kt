941f28d3e45bdccf3b3066dfb3b3a8b4deb8c294
palamarchukhome <palamarchuk.office@gmail.com>
2025-11-15 22:07:56 +0200

    Simplify muscles across app, all additional info will come with Json from api.

--- composeApp/src/commonMain/kotlin/org/trainingsession/project/domain/models/muscles/AccessoryMuscle.kt
+++ composeApp/src/commonMain/kotlin/org/trainingsession/project/domain/models/muscles/AccessoryMuscle.kt
@@ -1,24 +1,8 @@
 package org.trainingsession.project.domain.models.muscles
 
-sealed interface AccessoryMuscle : MuscleGroup {
-
-    val categoryName: String
-        get() = "Accessory Muscle & Stabilizers"
-
-    object ROTATOR_CUFF : AccessoryMuscle {
-        override val name: String = "Rotator Cuff"
-    } // Вращательная манжета плеча (для стабилизации)
-
-    object NECK : AccessoryMuscle {
-        override val name: String = "Neck"
-    } // Мышцы шеи
-
-    object FOREARMS : AccessoryMuscle {
-        override val name: String = "Forearms (Flexors & Extensors)"
-    } // Предплечья (сгибатели и разгибатели)
-
-    object GRIP_MUSCLES : AccessoryMuscle {
-        override val name: String = "Hand & Grip Muscles"
-    } // Мелкие мышцы кисти для силы хвата
-
+sealed interface AccessoryMuscleAndStabilizer : MuscleGroup {
+    object ROTATOR_CUFF : AccessoryMuscleAndStabilizer
+    object NECK : AccessoryMuscleAndStabilizer
+    object FOREARMS_FLEXORS_EXTENSORS : AccessoryMuscleAndStabilizer
+    object GRIP_MUSCLES : AccessoryMuscleAndStabilizer
 }
